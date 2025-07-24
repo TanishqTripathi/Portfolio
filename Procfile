03ee295378a80d83adb17449fe7e34edc434120f
Tanishq Kumar Tripathi <tanishqkumartripathi1110@gmail.com>
2025-07-24 12:02:55 +0530

    Moved files from Portfolio-website/ to root and cleaned structure

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: java -jar target/Portfolio-website-0.0.1-SNAPSHOT.jar
+
--- src/main/java/com/portfolio/Portfolio/website/Repository/MessageRepo.java
+++ src/main/java/com/portfolio/Portfolio/website/Repository/MessageRepo.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.portfolio.Portfolio.website.Model.Message;
 
 public interface MessageRepo extends JpaRepository<Message, Long> {
-    // This interface will automatically provide CRUD operations for the Message entity
+    // This interface will automatically provide CRUD operations for the Message
+    // entity
     // No additional methods are needed unless custom queries are required
-    // 
+    //
 
 }
